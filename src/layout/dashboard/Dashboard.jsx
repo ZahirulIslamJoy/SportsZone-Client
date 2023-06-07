d7f482715be65024506dd92f4cb0ced37dee24e2
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-06-07 23:45:58 +0600

    make the dashboard conditional based on diffreent user

--- src/layout/dashboard/Dashboard.jsx
+++ src/layout/dashboard/Dashboard.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
+  const isAdmin = true;
+  const isStudent = false;
+  const isInstructor = false;
+
   return (
     <div>
       <h1>Hello EveryOne</h1>
@@ -26,36 +31,49 @@ const Dashboard = () => {
 
         <aside
           id="default-sidebar"
-          className="fixed   lg:top-[60px] left-0 z-40  lg:w-[10%] h-screen transition-transform -translate-x-full sm:translate-x-0"
+          className="fixed   lg:top-[60px] left-0 z-40  h-screen transition-transform -translate-x-full sm:translate-x-0"
           aria-label="Sidebar"
         >
           <div className="h-full px-3 py-4 overflow-y-auto bg-[#1e2a4a] ">
-            <ul className="space-y-2 font-medium">
-              <li>
-                <a className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
-                  <span className="ml-3">Dashboard</span>
-                </a>
-              </li>
-              <li>
-                <a className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
-                  <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-                </a>
-              </li>
-              <li>
-                <a className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
-                  <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-                </a>
-              </li>
-              <li>
-                <a className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
-                  <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
-                </a>
-              </li>
-            </ul>
+            <div className="space-y-2 font-medium">
+              <Link className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
+                <span className="ml-3">Dashboard</span>
+              </Link>
+              {isAdmin && (
+                <div>
+                  <Link className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
+                    <span className="ml-3">Manage Classes</span>
+                  </Link>
+                  <Link className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
+                    <span className="ml-3">Manage Users</span>
+                  </Link>
+                </div>
+              )}
+              {isStudent && (
+                <div>
+                  <Link className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
+                    <span className="ml-3">Selected Classes</span>
+                  </Link>
+                  <Link className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
+                    <span className="ml-3">Enrolled Classes</span>
+                  </Link>
+                </div>
+              )}
+              {isInstructor && (
+                <div>
+                  <Link className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
+                    <span className="ml-3">Add Class</span>
+                  </Link>
+                  <Link className="flex items-center p-2 rounded-lg text-white hover:bg-gray-500">
+                    <span className="ml-3">My Classes</span>
+                  </Link>
+                </div>
+              )}
+            </div>
           </div>
         </aside>
-        <div >
-          <div className="p-4 sm:ml-32">
+        <div>
+          <div className="p-4 sm:ml-64">
             <h1>Outl</h1>
           </div>
         </div>
