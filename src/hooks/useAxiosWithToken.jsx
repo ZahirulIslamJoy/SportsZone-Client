2eb50e4cfd81d1f85f87e7482914f450dc91539a
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-06-08 15:06:33 +0600

    use axios intercept for verify user token

---
+++ src/hooks/useAxiosWithToken.jsx
@@ -0,0 +1,41 @@
+import { useContext, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProviders";
+
+const axiosSecure = axios.create({
+  baseURL: `${import.meta.env.VITE_URL}`,
+});
+
+const useAxiosWithToken = () => {
+  const { logOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem("access-token");
+  useEffect(() => {
+    axiosSecure.interceptors.request.use((config) => {
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (
+          error.response &&
+          (error.response.status === 401 || error.response.status === 403)
+        ) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logOut, navigate, axiosSecure]);
+
+  return [axiosSecure];
+};
+
+export default useAxiosWithToken;
