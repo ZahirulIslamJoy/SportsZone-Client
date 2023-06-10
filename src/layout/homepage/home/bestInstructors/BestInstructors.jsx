f898924458d4f1f115ea294aaad2a891f272785b
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-06-11 00:27:57 +0600

    completed the footer

--- src/layout/homepage/home/bestInstructors/BestInstructors.jsx
+++ src/layout/homepage/home/bestInstructors/BestInstructors.jsx
@@ -14,12 +14,12 @@ const BestInstructors = () => {
   console.log(popularInstructors);
 
   return (
-    <div className="bg-[#282A35] ">
+    <div className="bg-[#282A35] pb-32 ">
       <div className="w-[85%] mx-auto ">
         <h1 className="text-3xl text-white pt-16 mb-20 text-center">
           Our Popular Instructors
         </h1>
-        <div className="grid gap-6 grid-cols-3">
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {popularInstructors.map((singleInstructor) => (
             <div
               key={singleInstructor?._id}
--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from '../shared/Nav';
 import { Outlet } from 'react-router-dom';
+import Footer from '../shared/Footer';
 
 const Main = () => {
     return (
         <div>
             <Nav></Nav>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
