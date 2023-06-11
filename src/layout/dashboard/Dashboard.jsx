bb300bb2d877588ca5d78f85589c3bd4921caba2
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-06-11 17:08:46 +0600

    changed the dashboard design

--- src/layout/dashboard/Dashboard.jsx
+++ src/layout/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import useIsAdmin from "../../hooks/useIsAdmin";
 import useIsInstructor from "../../hooks/useIsInstructor";
@@ -8,151 +8,120 @@ import { AuthContext } from "../../providers/AuthProviders";
 import Marquee from "react-fast-marquee";
 
 const Dashboard = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isAdmin] = useIsAdmin();
   const [isInstructor] = useIsInstructor();
   const [isStudent] = useIsStudent();
   const { user } = useContext(AuthContext);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
-      <div className="h-full lg:hidden px-6  overflow-y-auto bg-[#1e2a4a] ">
-        <h1 className="text-2xl mt-4 mb-4 underline text-gray-200">
-          Dashboard Menus
-        </h1>
-        <div className=" font-medium">
-          {isAdmin && (
-            <div>
-              <Link
-                to="manageclasses"
-                className="flex items-center p-2 rounded-lg text-white "
+      <div className="flex  flex-col md:flex-row h-full">
+        {/* Sidebar */}
+        <aside
+          className={`bg-gray-900 text-gray-400 w-36 lg:w-52 flex-shrink-0 overflow-x-hidden  ${
+            isMenuOpen ? "block" : "hidden"
+          } md:block`}
+        >
+          <div className="flex items-center justify-center h-10 bg-gray-700">
+            <Marquee>
+              <span className="text-white text-sm font-semibold">
+                {user?.displayName}
+              </span>
+            </Marquee>
+          </div>
+          <nav className="mt-8">
+            <a className="flex items-center mt-4 py-2 px-6 text-gray-400 hover:bg-gray-700 hover:text-white">
+              <svg
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <span className="ml-3">Manage Classes</span>
-              </Link>
-              <Link
-                to="manageusers"
-                className="flex items-center p-2 rounded-lg text-white "
-              >
-                <span className="ml-3">Manage Users</span>
-              </Link>
-            </div>
-          )}
-          {isStudent && (
-            <div>
-              <Link
-                to="selectedclass"
-                className="flex items-center p-2 rounded-lg text-white "
-              >
-                <span className="ml-3">Selected Classes</span>
-              </Link>
-              <Link
-                to="enrollclass"
-                className="flex items-center p-2 rounded-lg text-white "
-              >
-                <span className="ml-3">Enrolled Classes</span>
-              </Link>
-              <Link
-                to="paymenthistory"
-                className="flex items-center p-2 rounded-lg text-white "
-              >
-                <span className="ml-3">Payment History</span>
-              </Link>
-            </div>
-          )}
-          {isInstructor && (
-            <div>
-              <Link
-                to="addclass"
-                className="flex items-center p-2 rounded-lg text-white "
-              >
-                <span className="ml-3">Add Class</span>
-              </Link>
-              <Link
-                to="myclass"
-                className="flex items-center p-2 rounded-lg text-white "
-              >
-                <span className="ml-3">My Classes</span>
-              </Link>
-            </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              </svg>
+              <span className="mx-4">Dashboard</span>
+            </a>
+            {isStudent && (
+              <span>
+                <Link to='selectedclass'>
+                  <span className="flex items-center mt-4 py-2 px-6 text-gray-400 hover:bg-gray-700 hover:text-white">
+                    <span className="mx-4">Selected Classes</span>
+                  </span>
+                </Link>
+                <Link to='enrollclass'>
+                  <span className="flex items-center mt-4 py-2 px-6 text-gray-400 hover:bg-gray-700 hover:text-white">
+                    <span className="mx-4">Enrolled Classes</span>
+                  </span>
+                </Link>
+                <Link to='paymenthistory'>
+                  <span className="flex items-center mt-4 py-2 px-6 text-gray-400 hover:bg-gray-700 hover:text-white">
+                    <span className="mx-4">Payment History</span>
+                  </span>
+                </Link>
+              </span>
+            )}
+            {isInstructor && (
+              <span>
+                <Link to='addclass'>
+                  <span className="flex items-center mt-4 py-2 px-6 text-gray-400 hover:bg-gray-700 hover:text-white">
+                    <span className="mx-4">Add Class</span>
+                  </span>
+                </Link>
+                <Link to='myclass'>
+                  <span className="flex items-center mt-4 py-2 px-6 text-gray-400 hover:bg-gray-700 hover:text-white">
+                    <span className="mx-4">My Class</span>
+                  </span>
+                </Link>
+              </span>
+            )}
+             {isAdmin && (
+              <span>
+                <Link to='manageclasses'>
+                  <span className="flex items-center mt-4 py-2 px-6 text-gray-400 hover:bg-gray-700 hover:text-white">
+                    <span className="mx-4">Manage Class</span>
+                  </span>
+                </Link>
+                <Link to='manageusers'>
+                  <span className="flex items-center mt-4 py-2 px-6 text-gray-400 hover:bg-gray-700 hover:text-white">
+                    <span className="mx-4">Manage Users</span>
+                  </span>
+                </Link>
+              </span>
+            )}
+          </nav>
+        </aside>
+        {/* Mobile Menu */}
+        <div className="md:hidden">
+          {!isMenuOpen ? (
+            <button
+              className="bg-gray-800 text-gray-400  text-left"
+              onClick={toggleMenu}
+            >
+              Dahboard
+            </button>
+          ) : (
+            <button
+              className="flex items-center justify-between px-4 py-3 text-gray-500 hover:text-white"
+              onClick={toggleMenu}
+            >
+              Close
+            </button>
           )}
         </div>
-      </div>
-      <div>
-        <aside
-          id="default-sidebar"
-          className=" fixed  lg:top-[67px] left-0 z-40 overflow-y-auto transition-transform -translate-x-full sm:translate-x-0"
-          aria-label="Sidebar"
-        >
-          <div className=" bg-gray-300  h-full px-3 py-4 overflow-y-auto text-black  ">
-            <div className="space-y-2 font-medium">
-              {isAdmin && (
-                <div>
-                  <Link
-                    to="manageclasses"
-                    className="flex items-center p-2 rounded-lg text-black "
-                  >
-                    <span className="ml-3">Manage Classes</span>
-                  </Link>
-                  <Link
-                    to="manageusers"
-                    className="flex items-center p-2 rounded-lg text-black "
-                  >
-                    <span className="ml-3">Manage Users</span>
-                  </Link>
-                </div>
-              )}
-              {isStudent && (
-                <div>
-                  <Link
-                    to="selectedclass"
-                    className="flex items-center p-2 rounded-lg text-black "
-                  >
-                    <span className="ml-3">Selected Classes</span>
-                  </Link>
-                  <Link
-                    to="enrollclass"
-                    className="flex items-center p-2 rounded-lg text-black "
-                  >
-                    <span className="ml-3">Enrolled Classes</span>
-                  </Link>
-                  <Link
-                    to="paymenthistory"
-                    className="flex items-center p-2 rounded-lg text-black "
-                  >
-                    <span className="ml-3">Payment History</span>
-                  </Link>
-                </div>
-              )}
-              {isInstructor && (
-                <div>
-                  <Link
-                    to="addclass"
-                    className="flex items-center p-2 rounded-lg text-white "
-                  >
-                    <span className="ml-3">Add Class</span>
-                  </Link>
-                  <Link
-                    to="myclass"
-                    className="flex items-center p-2 rounded-lg text-white "
-                  >
-                    <span className="ml-3">My Classes</span>
-                  </Link>
-                </div>
-              )}
-            </div>
-          </div>
-        </aside>
-        <div>
-          <div className="bg-gray-100">
-            <div className="p-4 w-[85%]  mx-auto">
-              <Marquee speed={120}>
-                <h1 className=" text-2xl lg:text-4xl uppercase mt-12 mb-16 text-center ">
-                  WELCOME! <span> {user?.displayName}</span>
-                </h1>
-              </Marquee>
-
-              <Outlet></Outlet>
-            </div>
-          </div>
+        {/* Content */}
+        <div className="flex flex-col ml-12 overflow-x-auto flex-1">
+          <h1 className="text-3xl text-center mt-12 mb-12">Welcome!!!</h1>
+          <Outlet></Outlet>
         </div>
       </div>
     </div>
